4e8cea31d272cedf7f7e2d4824c99d689741bec4
阳林 <yl25488@ly.com>
2018-04-04 18:14:13 +0800

    【修改】

---
+++ Macrosage.BaiduAI/ImageClassify.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Macrosage.BaiduAI
+{
+    /// <summary>
+    /// 图像识别
+    /// </summary>
+    public class ImageClassify
+    {
+        /// <summary>
+        /// 植物识别
+        /// </summary>
+        /// <param name="filesrc"></param>
+        public JObject PlantDetect(string filesrc)
+        {
+            var client = new Baidu.Aip.ImageClassify.ImageClassify(ApiConfig.APIKey, ApiConfig.SecretKey);
+            var image = File.ReadAllBytes(filesrc);
+            // 调用植物识别，可能会抛出网络等异常，请使用try/catch捕获
+            var result = client.PlantDetect(image);
+            return result;
+        }
+    }
+}
---
+++ Macrosage.Face.Web/Controllers/tech/ImageRecognitionController.cs
@@ -0,0 +1,22 @@
+﻿using Macrosage.BaiduAI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Macrosage.Face.Web.Controllers.tech
+{
+    /// <summary>
+    /// 图像识别
+    /// </summary>
+    public class ImageRecognitionController : Controller
+    {
+        private readonly static ImageClassify imageClassify = new ImageClassify();
+        // GET: ImageRecognition
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
